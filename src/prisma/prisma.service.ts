ea5aef7451d1ed8699e537b4292266847bb5d220
lipschitzezekiel-cyber <lipschitzezekiel@gmail.com>
2026-06-27 04:03:58 +0100

    fix prisma client for render

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,25 +1,38 @@
 import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
   private readonly logger = new Logger(PrismaService.name);
+  private client: any;
 
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
     });
-    super({ adapter });
+    const { PrismaClient } = require('@prisma/client');
+    this.client = new PrismaClient({ adapter });
   }
 
+  get user() { return this.client.user; }
+  get vendor() { return this.client.vendor; }
+  get transaction() { return this.client.transaction; }
+  get webhookEvent() { return this.client.webhookEvent; }
+  get payout() { return this.client.payout; }
+  get checkoutSession() { return this.client.checkoutSession; }
+  get otp() { return this.client.otp; }
+
+  async $queryRaw(...args: any[]) { return this.client.$queryRaw(...args); }
+  get $connect() { return this.client.$connect.bind(this.client); }
+  get $disconnect() { return this.client.$disconnect.bind(this.client); }
+
   async onModuleInit() {
-    await this.$connect();
+    await this.client.$connect();
     this.logger.log('Database connected successfully');
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    await this.client.$disconnect();
     this.logger.log('Database disconnected');
   }
 }
